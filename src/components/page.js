259100ebcc7af88c5be46d4cd5aec4c8a5863739
kinanti09 <kinantianjang09@gmail.com>
2019-09-16 16:25:22 +0700

    layout

--- src/components/page.js
+++ src/components/page.js
@@ -4,7 +4,7 @@ import SearchBar from './searchBar';
 
 const Page = () => {
     return (
-        <div>
+        <div className="ui container" style={{ marginTop: '10px' }}>
             <SearchBar />
         </div>
     );
